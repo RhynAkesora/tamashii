7c0e7f8c633c82b66ea09afb2af5cb58997b0056
RhynAkesora <archmetal.255@gmail.com>
2025-07-19 03:32:03 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'tamashii-tetris-v052';
+const urlsToCache = [
+  './',
+  './index.html',
+  './yoga01.png',
+  './soul-stage.mp3',
+  './soul-gameover.mp3'
+];
+
+// インストール時にキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// リクエスト時にキャッシュから返す
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
