270ef65059444ff55e00d10a5bdd6490046e1b47
RhynAkesora <archmetal.255@gmail.com>
2026-02-21 02:38:04 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -5,18 +5,35 @@ const urlsToCache = [
   './yoga01.png',
   './game-title.png',
   './logo-white.png',
+  './icon-192.png',
+  './icon-512.png',
   './soul-stage.mp3',
   './soul-gameover.mp3',
-  './fever-bgm.mp3',
+  './soul-title.mp3',
+  './soul-highscore.mp3',
   './soul-space.mp3',
+  './fever-bgm.mp3',
   './flash.mp3'
 ];
 
-// インストール時にキャッシュ
+// インストール時にキャッシュ（即座に有効化）
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(
+        names.filter((name) => name !== CACHE_NAME)
+             .map((name) => caches.delete(name))
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
